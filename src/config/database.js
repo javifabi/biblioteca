d0a9ba4dc927fdba14499f59d3744510fa4f4fed
javifabi <j.fabia@alumnos.santotomas.cl>
2026-06-15 10:46:46 -0400

    Usar MYSQL_URL para Railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,23 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.MYSQLDATABASE || process.env.DB_NAME,
-    process.env.MYSQLUSER || process.env.DB_USER,
-    process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-    {
-        host: process.env.MYSQLHOST || process.env.DB_HOST,
-        port: process.env.MYSQLPORT || 3306,
+let sequelize;
+
+if (process.env.MYSQL_URL) {
+    sequelize = new Sequelize(process.env.MYSQL_URL, {
         dialect: "mysql"
-    }
-);
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.MYSQLDATABASE || process.env.DB_NAME,
+        process.env.MYSQLUSER || process.env.DB_USER,
+        process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+        {
+            host: process.env.MYSQLHOST || process.env.DB_HOST,
+            port: process.env.MYSQLPORT || 3306,
+            dialect: "mysql"
+        }
+    );
+}
 
 module.exports = sequelize;
